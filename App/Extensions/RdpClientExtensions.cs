1f91b08a538f81cf302b4eab6ac5d48a34be1261
nevermindjq <pavelgoryajnov@yandex.com>
2024-06-27 09:46:15 +0300

    Initial commit

---
+++ App/Extensions/RdpClientExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxMSTSCLib;
+
+namespace App.Extensions
+{
+    public static class RdpClientExtensions
+    {
+        public static async Task<bool> Check(this AxMsRdpClient8NotSafeForScripting rdp, string server, Point window_position, string directory, int? port = null) {
+            rdp.Server = server;
+
+            if (port is not null) {
+                rdp.AdvancedSettings8.RDPPort = port.Value;
+            }
+
+            rdp.Connect();
+
+            await Task.Delay(3000);
+
+            if (rdp.Connected != 1) {
+                return false;
+            }
+            
+            rdp.Focus();
+
+            for (int i = 0; i < 10; i++)
+            {
+                SendKeys.Send("+");
+            }
+
+            await Task.Delay(1000);
+
+            var margin = new Point {
+                X = rdp.Margin.Left + rdp.Margin.Right,
+                Y = rdp.Margin.Top + rdp.Margin.Bottom
+            };
+
+            using (var map = new Bitmap(rdp.Width - margin.X, rdp.Height - margin.Y)) {
+                using (var graphics = Graphics.FromImage(map)) {
+                    graphics.CopyFromScreen(
+                        window_position.X + rdp.Bounds.X - margin.X,
+                        window_position.Y + rdp.Bounds.Y - margin.Y,
+                        0,
+                        0,
+                        map.Size,
+                        CopyPixelOperation.SourceCopy
+                    );
+                }
+
+                map.Save(Path.Combine(directory, $"{rdp.Server}.png"));
+            }
+
+            rdp.Disconnect();
+
+            await Task.Delay(2000);
+
+            return true;
+        }
+    }
+}
---
+++ App/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+using App.Extensions;
+
+namespace App {
+	public partial class Form1 : Form {
+		private CancellationTokenSource _source = new();
+		
+		public Form1() {
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e) {
+			rdp.AdvancedSettings8.AuthenticationLevel = 0;
+			
+			base.OnLoad(e);
+		}
+
+		private async void btn_pick_Click(object sender, EventArgs e) {
+			var dialog = new OpenFileDialog {
+				Filter = "Text|*.txt",
+				Multiselect = false
+			};
+
+			if (dialog.ShowDialog() is not DialogResult.OK) {
+				return;
+			}
+
+			var info = new FileInfo(dialog.FileName);
+			var filename = info.Name.Substring(0, info.Name.Length - info.Extension.Length);
+
+			Directory.CreateDirectory(filename);
+			
+			using (var reader = new StreamReader(dialog.FileName)) {
+				while (!reader.EndOfStream && !_source.Token.IsCancellationRequested) {
+					var line = (await reader.ReadLineAsync()).Split(':');
+					
+					await rdp.Check(
+						line[0],
+						new() {
+							X = Location.X + Width - ClientRectangle.Width,
+							Y = Location.Y + Height - ClientRectangle.Height
+						},
+						filename,
+						line.Length == 2 && int.TryParse(line[1], out var port) ? port : null
+					);
+				}
+			}
+		}
+
+		private void btn_cancel_Click(object sender, EventArgs e) {
+			_source.Cancel();
+		}
+	}
+}
